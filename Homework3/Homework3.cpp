8f8ddea8253a1f5459eb57908f5e117e826403dd
Jester <kj.provenzano@gmail.com>
2015-11-03 21:16:17 -0500

    StopLight class is almost done with the exception on yellow chaninging lights

--- Homework3/Homework3.cpp
+++ Homework3/Homework3.cpp
@@ -8,19 +8,31 @@
 
 using namespace std;
 
+const int NUM_OF_LIGHTS = 4;
+
 time_t startTime = time(0);
+
+enum direction {
+	north = 0 , south = 1 , east = 2 , west = 3
+};
 
 class StopLight {
 
 public:
-	StopLight(string direction) {
+	StopLight(direction direction) {
 		this->lightColor = "";
 		this->direction = direction;
 	}
 
-	StopLight(string color , string direction) {
+	StopLight(string color , direction direction) {
 		this->lightColor = color;
 		this->direction = direction;
+	}
+
+	StopLight(string color , direction direction , StopLight *nextLight) {
+		this->lightColor = color;
+		this->direction = direction;
+		this->nextLight = nextLight;
 	}
 
 	string getLightColor() {
@@ -31,7 +43,7 @@ public:
 		this->lightColor = c;
 	}
 
-	string getDirection() {
+	direction getDirection() {
 		return this->direction;
 	}
 
@@ -40,36 +52,77 @@ public:
 	}
 
 	void cycleLights() {
+		this->lightColor = this->lightColor == "red" ? "green" : "red";
+		
+		/*switch(this->direction) {
+		case north:
+		case south:
+			this->lightColor = this->lightColor == "red" ? "green" : "red";
+			break;
+		case east:
+		case west:
+			this->lightColor = this->lightColor == "red" ? "green" : "red";
+			break;
+		}*/
 
 		if(this->nextLight != NULL) {
-			this->nextLight->lightColor = "green";
 			this->nextLight->cycleLights();
-		}		
+		}
+	}
+
+	static void emergencyChange(direction dir , StopLight (&lights)[NUM_OF_LIGHTS]) {
+		for(int i = 0; i < NUM_OF_LIGHTS; i++) {
+			if(lights[i].direction == dir) 
+				lights[i].setLightColor("green");
+			else 
+				lights[i].setLightColor("red");
+		}
+	}
+
+	static void resetLights(StopLight (&lights)[NUM_OF_LIGHTS]) {
+		for(int i = 0 ; i < NUM_OF_LIGHTS ; i++) {
+			switch(lights[i].direction) {
+			case north:
+			case south:
+				lights[i].setLightColor("green");
+				break;
+			case east:
+			case west:
+				lights[i].setLightColor("red");
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void printLightColor() {
+		cout << this->lightColor;
 	}
 private:
 	string lightColor;
 	StopLight *nextLight;
-	string direction;
+	direction direction;
 };
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	StopLight north("red");
-	StopLight south("red");
-	StopLight east("red");
-	StopLight west("red");
+int _tmain(int argc , _TCHAR* argv []) {
+	StopLight lights [] = {
+		StopLight("green" , direction::north , &lights[1]) ,
+		StopLight("green" , direction::south , &lights[2]) ,
+		StopLight("red" , direction::west , &lights[3]) ,
+		StopLight("red" , direction::east , NULL)
+	};
 
-	north.setNextLight(&south);
-	south.setNextLight(&west);
-	west.setNextLight(&east);
-	east.setNextLight(NULL);
+	lights[0].cycleLights();
 
-	north.cycleLights();
+	StopLight::emergencyChange(direction::north , lights);
+	//StopLight::resetLights(lights);
 
-	cout << north.getLightColor() << endl;
-	cout << south.getLightColor() << endl;
-	cout << east.getLightColor() << endl;
-	cout << west.getLightColor() << endl;
+	for each (auto var in lights) {
+		cout << var.getDirection() << ": ";
+		var.printLightColor();
+		cout << endl;
+	}
 
 	return 0;
 }
